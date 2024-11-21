815ca497721375dd920ec836f019e77d0bf3f44e
ananya-3007 <ananya.muger@gmail.com>
2024-11-21 17:22:20 +0530

    Blog Creation

---
+++ blog/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+def home(request):
+    return HttpResponse("""<h1> Welcome to my Blog </h1>
+                        <p> Hope you are doing well.</p>
+                        <ol>
+                        <li> Apple</li>
+                        <li> Mango</li>
+                        <li> Chikku</li>
+                        </ol>"""
+                        )
+# Create your views here.
+def about(request):
+    return HttpResponse('<h1>About</h1>')
